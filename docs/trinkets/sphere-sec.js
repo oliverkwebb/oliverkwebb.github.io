0d9e76d678d45ec7107e3e223a175359e525df07
Oliver Webb <oliverwebb8192@gmail.com>
2025-05-25 09:41:09 -0500

    sphere bug fix 2

--- docs/trinkets/sphere-sec.js
+++ docs/trinkets/sphere-sec.js
@@ -21,7 +21,7 @@ let sphere_sec_inp2 = document.querySelector("#sphere-sec-2");
 let sphere_sec_inp3 = document.querySelector("#sphere-sec-3");
 let sphere_sec_inp4 = document.querySelector("#sphere-sec-4");
 
-function sphere_sec_angbetween(a1, d1, a2, d2) {
+function sphere_sec_angbetween(d1, a1, d2, a2) {
 	return Math.acos(Math.cos(a1-a2) * Math.cos(d1) * Math.cos(d2) + Math.sin(d1) * Math.sin(d2));
 }
 
@@ -31,16 +31,16 @@ function render_sphere_sec(ssrdiv, ang1, ang2, ang3, ang4) {
 	let a3 = ang3 * (Math.PI/180);
 	let a4 = ang4 * (Math.PI/180);
 	let drad  = sphere_sec_angbetween(a1, a2, a3, a4);
-	let d = drad * (Math.PI / 180);
+	let d = drad / (Math.PI / 180);
 	let crd  = 2 * Math.sin(drad/2);
 	ssrdiv.innerHTML = `
 	<table>
 	<tr><td>LatLong X<td> ${ang1.toFixed(2)}, ${ang2.toFixed(2)}</td></tr>
 	<tr><td>LatLong Y<td> ${ang3.toFixed(2)}, ${ang4.toFixed(2)}</td></tr>
-	<tr><td>Distance (Radii)</td><td>${d.toFixed(2)}</td>
-	<tr><td>Chord</td><td>${crd.toFixed(2)}</td></tr>
+	<tr><td>Circumference Distance (Radii)</td><td>${drad.toFixed(2)}</td>
+	<tr><td>Chord Distance (Radii)</td><td>${crd.toFixed(2)}</td></tr>
 	<tr><td>Ratio</td><td>${(drad/crd).toFixed(3)}</td></tr>
-	<tr><td>Distance (Earth, km)</td><td>${(crd*6371).toFixed(2)}km</td></tr>
+	<tr><td>Chord Distance (Earth, km)</td><td>${(crd*6371).toFixed(2)}km</td></tr>
 	</table>
 	`
 }
